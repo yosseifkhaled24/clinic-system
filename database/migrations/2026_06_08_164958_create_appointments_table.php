fde4ad84b8410b767a858f97f6d08439b93911a1
yosseifkhsled24 <yosseifkhaled8@gmail.com>
2026-06-08 20:30:20 +0300

    Complete database design and migrations

---
+++ database/migrations/2026_06_08_164958_create_appointments_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+ public function up(): void
+{
+    Schema::create('appointments', function (Blueprint $table) {
+        $table->id();
+
+        $table->foreignId('user_id')
+              ->constrained()
+              ->cascadeOnDelete();
+
+        $table->foreignId('doctor_id')
+              ->constrained()
+              ->cascadeOnDelete();
+
+        $table->dateTime('appointment_date');
+
+        $table->enum('status', [
+            'pending',
+            'approved',
+            'completed',
+            'cancelled'
+        ])->default('pending');
+
+        $table->text('notes')->nullable();
+
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
